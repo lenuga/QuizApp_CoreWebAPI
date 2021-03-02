554c4ee450bf131be85388b9b32395ea2fef1006
Lenuga <lenuga21cis@gamil.com>
2021-03-02 21:31:23 +0530

    Add project files.

---
+++ QuizApp_CoreWebAPI/Migrations/20210302041446_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizApp_CoreWebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    userId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    emailId = table.Column<string>(maxLength: 255, nullable: true),
+                    address = table.Column<string>(maxLength: 255, nullable: true),
+                    phoneNo = table.Column<string>(maxLength: 50, nullable: true),
+                    userType = table.Column<string>(maxLength: 50, nullable: true),
+                    firstName = table.Column<string>(maxLength: 255, nullable: true),
+                    password = table.Column<string>(maxLength: 255, nullable: true),
+                    lastName = table.Column<string>(maxLength: 255, nullable: true),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__UserInfo__CB9A1CFF43C258F2", x => x.userId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
---
+++ QuizApp_CoreWebAPI/Models/OnlineQuizDBContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace QuizApp_CoreWebAPI.Models
+{
+    public partial class OnlineQuizDBContext : DbContext
+    {
+        
+
+        public OnlineQuizDBContext(DbContextOptions<OnlineQuizDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserInfo> UserInfo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__CB9A1CFF43C258F2");
+
+                entity.Property(e => e.UserId).HasColumnName("userId");
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("address")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.EmailId)
+                    .HasColumnName("emailId")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.FirstName)
+                    .HasColumnName("firstName")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.PhoneNo)
+                    .HasColumnName("phoneNo")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UserType)
+                    .HasColumnName("userType")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .HasColumnName("lastName")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Password)
+                 .HasColumnName("password")
+                 .HasMaxLength(255);
+
+                entity.Property(e => e.Username)
+                .HasColumnName("username")
+                .HasMaxLength(255);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
